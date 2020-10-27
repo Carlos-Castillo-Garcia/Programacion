e1978428a930e89df9f3198433d4217e2d26f15e
CARLOS <CARLOS@DESKTOP-BGQH1KP>
2020-10-27 14:36:24 +0100

    hasta el ejercicio 20 terminado

--- Programacion/src/UT3/ejercicios_teams/ejercicio20.java
+++ Programacion/src/UT3/ejercicios_teams/ejercicio20.java
@@ -19,12 +19,16 @@ public class ejercicio20 {
 		int numero_usuario;
 		int suma_de_numeros;
 		int aux;
+		int media;
 		int total_suma = 0;
+		int contador = -1;
 
 
 		System.out.println("Para parar este programa de suma tiene que poner un 0");
 
 		do{
+			
+			contador = contador + 1;
 			
 			System.out.print("Introduzca un numero: ");
 			numero_usuario = entrada.nextInt();
@@ -32,9 +36,13 @@ public class ejercicio20 {
 			suma_de_numeros = aux + total_suma;
 			total_suma = suma_de_numeros;
 
+			
 		}while(numero_usuario != 0);
 
 		System.out.println("Esta es la suma de los numeros introducidos: " +suma_de_numeros );
+		
+		media = suma_de_numeros / contador;
+		System.out.println("Esta es la media: " +media);
 		
 		entrada.close();
     } 
