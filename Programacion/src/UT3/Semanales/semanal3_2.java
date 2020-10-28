6d09b10c8a8ee9a304d26164b6208b1bb10dc042
carlos.cg <carlos.cg@DESKTOP-3K5TKR4.localdomain>
2020-10-28 11:34:28 +0100

    semanal 3 encondiciones

--- Programacion/src/UT3/Semanales/semanal3_2.java
+++ Programacion/src/UT3/Semanales/semanal3_2.java
@@ -16,12 +16,12 @@ public class semanal3_2 {
 		// TODO Auto-generated method stub
 		Scanner entrada =new Scanner(System.in);
 		
-		int real;
+		int superficie;
 		int estimacion;
 		int minimo;
 		int maximo;
-		int division = 0;
-		//int salir;
+		int division;
+
 		
 		minimo = 90 * 45;
 		maximo = 120 * 90;
@@ -29,34 +29,33 @@ public class semanal3_2 {
 		
 			do {
 
-
+				System.out.print("Introduzca la medida superficie en metros cuadrados: ");
+				superficie = entrada.nextInt();
 				
-				if(division >= minimo && division <= maximo ) {
-					System.out.println("SI, la estimacion es correcta");
+				if(superficie !=0) {
+
+					System.out.print("Introduzca la estimacion en campos de futbol: ");
+					estimacion = entrada.nextInt();
+					
+					if(estimacion != 0) {
+						
+						division=superficie/estimacion;
+						
+						if(division >= minimo && division <= maximo ) {
+							System.out.println("SI, la estimacion es correcta");
+						}else {
+							System.out.println("NO, la estimacion no es correcta");
+						}
+						
+					}else {
+						System.out.println("la estimacion no puede ser un cero");
+					}
+					
 				}else {
-					System.out.println("NO, la estimacion no es correcta");
+					System.out.println("hasta luego");
 				}
-				
 
-				
-				System.out.print("Introduzca la estimacion en campos de futbol: ");
-				estimacion = entrada.nextInt();
-				
-				System.out.print("Introduzca la medida real en metros cuadrados: ");
-				real = entrada.nextInt();
-				
-				division=real/estimacion;
-////Salir-------------------------------------------------------------------------------------------------------
-//				System.out.println("¿Desea salir del programa? ( 1 = No ; 0 = Si )");
-//				real = entrada.nextInt();
-//
-//				if(real == 0) {
-//					System.out.println("Hasta luego");
-//				}else {
-//					//System.out.println("Vuelva a introducir el precio");
-//				}
-
-			}while(real != 0);
+			}while(superficie != 0);
 			
 			entrada.close();
 	}
