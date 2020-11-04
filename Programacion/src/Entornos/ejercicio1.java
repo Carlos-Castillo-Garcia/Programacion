4f92f8fc98136ccc3470ce1695c183fd649ef387
carlos.cg <carlos.cg@DESKTOP-3K5TKR4.localdomain>
2020-11-04 08:56:44 +0100

    ejercicio de entronos

---
+++ Programacion/src/Entornos/ejercicio1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Entornos;
+import java.util.Scanner;
+
+/**
+ * @author carlos.cg
+ *
+ */
+public class ejercicio1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		entrada.close();
+	}
+
+}
