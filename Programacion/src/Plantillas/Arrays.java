4684ad2dc0a2060cb52d81784db8536d19495956
carlos.cg <carlos.cg@DESKTOP-3K5TKR4.localdomain>
2020-11-16 13:45:48 +0100

    ejercicio 63 y arrays

---
+++ Programacion/src/Plantillas/Arrays.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Plantillas;
+import java.util.Scanner; 
+/**
+ * @author carlos.cg
+ *
+ */
+public class Arrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		int array1 [];
+		array1 = new int [3];
+			
+		entrada.close();
+	}
+
+}
--- Programacion/src/UT3/ejercicios_teams/Doc3/ejercicio63.java
+++ Programacion/src/UT3/ejercicios_teams/Doc3/ejercicio63.java
@@ -2,19 +2,37 @@
  * 
  */
 package UT3.ejercicios_teams.Doc3;
-
+import java.util.Scanner;
 /**
  * @author CARLOS
  *
  */
-public class ejercicio35 {
+public class ejercicio63 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		int numero[] = new int[10];	
+		int media = 0;
+		
+		
+		for(int cont = 0; cont < numero.length ; cont++) {
+			System.out.println("Introduzca un numero: ");
+			numero[cont] = entrada.nextInt();
+			}
+		
+		for(int cont = 0; cont < numero.length ; cont++) {
+			media = media + numero[cont];
+		
+			}
 
+		System.out.println("esta es la media: " +media/10);
+		
+		entrada.close();
 	}
 
 }
