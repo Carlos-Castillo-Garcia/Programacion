48755c82d32c4051cc2c9000618508e8a467f799
carlos.cg <carlos.cg@DESKTOP-3K5TKR4.localdomain>
2020-11-18 08:41:43 +0100

    ejercicio 49 terminado

--- Programacion/src/UT3/ejercicios_teams/Doc3/ejercicio49.java
+++ Programacion/src/UT3/ejercicios_teams/Doc3/ejercicio49.java
@@ -1,20 +1,28 @@
-/**
- * 
- */
 package UT3.ejercicios_teams.Doc3;
 
-/**
- * @author CARLOS
- *
- */
+import java.util.Scanner;
+
+
 public class ejercicio49 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);
+		long numero;           
+		long cifranueva=0;
+		System.out.println("ingrese el numero");
+		numero= entrada.nextLong(); 
+		long aux= numero; 
+		do {
+			numero = aux;
+			cifranueva = 0;
+			do{  
+				cifranueva=cifranueva + (numero % 10);
+				numero = numero/10;   	   
+				System.out.println(numero+" La suma de los digitos de "+aux+" es "+cifranueva); 
 
+			}while (numero != 0);
+			aux = cifranueva;
+		}while(cifranueva > 9);
 	}
-
 }
