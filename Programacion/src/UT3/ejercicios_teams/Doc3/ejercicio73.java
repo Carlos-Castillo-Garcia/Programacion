d95e6847e50b29fccfce86040b1479e0286821d9
Carlos-Castillo-Garcia <73341536+Carlos-Castillo-Garcia@users.noreply.github.com>
2020-12-14 17:44:27 +0100

    as

--- Programacion/src/UT3/ejercicios_teams/Doc3/ejercicio73.java
+++ Programacion/src/UT3/ejercicios_teams/Doc3/ejercicio73.java
@@ -2,19 +2,44 @@
  * 
  */
 package UT3.ejercicios_teams.Doc3;
-
+import java.util.Scanner;
 /**
  * @author CARLOS
  *
  */
-public class ejercicio35 {
+public class ejercicio73 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner(System.in);
+		
+		int array [] = new int [10];
+		int numero;
+		
+		for(int posi = 0; posi < array.length; posi++) {
+			System.out.println("Escriba un numero");
+			numero = entrada.nextInt();
+			
+			if(posi==0) {
+				array[posi]=numero;
+			}else {
+				for(int posi2 = 0; posi2<posi;posi2++) {
+					if(array[posi2]==numero) {
+						System.out.println("ya ha lo has introducido");
+						posi--;
+					}else {
+						array[posi]= numero;
+					}
+				}
+			}
+		}
+		
+		for(int cont = 0; cont<array.length;cont++) {
+			System.out.printf("\n%d ", array[cont]);
+		}
 	}
 
 }
