ab3343a009bd8a3f90dad8675a8bc8feec7b1806
Carlos-Castillo-Garcia <73341536+Carlos-Castillo-Garcia@users.noreply.github.com>
2021-01-12 13:45:49 +0100

    medio terminado el 80

--- Programacion/src/UT3/ejercicios_teams/Doc4/ejercicio80.java
+++ Programacion/src/UT3/ejercicios_teams/Doc4/ejercicio80.java
@@ -22,15 +22,21 @@ public class ejercicio80 {
 		int num1 = entrada.nextInt();
 		System.out.println("Escribe un numero: ");
 		int num2 = entrada.nextInt();
+		System.out.println("Escribe un numero: ");
+		int num3 = entrada.nextInt();
 		
 		int max = maximo(num1, num2);
 		
+		max = maximo2(num1, num2, num3);
+		
+		//max = maximoarray(num1, num2, num3);
+		
 		System.out.println("El numero mas alto es: "+max);
 		
-		System.out.println(""+max());
-		System.out.println(""+max);
-		System.out.println(""+max);
-		System.out.println(""+max);
+//		System.out.println(""+max);
+//		System.out.println(""+max);
+//		System.out.println(""+max);
+//		System.out.println(""+max);
 		
 		entrada.close();
 	}
@@ -51,22 +57,49 @@ public class ejercicio80 {
 		
 	}
 	
-	private static int maximo2(int num1, int num2) {
+	private static int maximo2(int num1, int num2, int num3) {
 		int max = num1;
-		
 
+		if (num1 > num2 && num1 > num3) {
+			max = num1;
+		}
 		
+		if (num2 > num1 && num2 > num3) {
+			max = num2;
+		}
+		
+		if (num3 > num1 && num3 > num2) {
+			max = num3;
+		}
+
 		return max;
-		
+
+	}
+
+	private static int maximoarray(int num1, int num2) {
+		int max = num1;
+
+
+
+		return max;
+
 	}
 	
-	private static int maximoarray(int , int num2) {
-		int max = num1;
-		
+    private static void rellena(int[][] tabla) {
+        for(int i=0; i<tabla.length; i++){
+            for(int j=0; j<tabla[i].length; j++) {
+                tabla[i][j]= (int) (Math.random()*10);
+            }
+        }
+    }
 
-		
-		return max;
-		
-	}
+    private static void pinta(int [][] tabla) {
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++) {
+                System.out.printf("%d \t",tabla[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 	
 }
