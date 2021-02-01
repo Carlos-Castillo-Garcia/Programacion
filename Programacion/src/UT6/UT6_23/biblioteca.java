e5a5f32efba866bd307df149155643da3055e49a
Manuel <Manuel@DESKTOP-QLAQ6VU>
2021-02-01 13:37:22 +0100

    mods ejercicio 23

--- Programacion/src/UT6/UT6_23/biblioteca.java
+++ Programacion/src/UT6/UT6_23/biblioteca.java
@@ -17,9 +17,9 @@ public class biblioteca {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner varstring = new Scanner(System.in);
-		Scanner varint = new Scanner(System.in);
-		Scanner varopcion = new Scanner(System.in);
+		Scanner scstring = new Scanner(System.in);
+		Scanner scint = new Scanner(System.in);
+		Scanner scopcion = new Scanner(System.in);
 
 		ArrayList<Libro>catalogo = new ArrayList<Libro>();
 		String titulo;
@@ -29,27 +29,20 @@ public class biblioteca {
 		int opcion = 0;
 
 		do {
-			System.out.println("Estas son las opciones a elegir del menu:");
-			System.out.println("[1] Dar de alta un libro");
-			System.out.println("[2] Consultar el listado de libros");
-			System.out.println("[3] Dar de baja un libro");
-			System.out.println("[4] Hacer un el prestamo de un libro");
-			System.out.println("[5] Hacer una devolucion");
-			System.out.println("[0] Salir del programa");
-			opcion = varopcion.nextInt();
+			opcion = menu(scopcion);
 			System.out.println("");
 			switch(opcion){
 			
 				case 1:
 					System.out.println("Has seleccionado la opcion de dar de alta un libro, \nIntroduzca los datos del libro a continuacion: \n");
 					System.out.println("Titulo del libro:");
-					titulo = varstring.nextLine();
+					titulo = scstring.nextLine();
 					System.out.println("\nAutor del libro:");
-					autor = varstring.nextLine();
+					autor = scstring.nextLine();
 					System.out.println("\nEjemplares del libro:");
-					ejemplares = varint.nextInt();
+					ejemplares = scint.nextInt();
 					System.out.println("\nLibros prestados:");
-					prestados = varint.nextInt();
+					prestados = scint.nextInt();
 	
 					if(ejemplares < prestados) {
 						prestados = 0;
@@ -62,7 +55,7 @@ public class biblioteca {
 				case 2:
 					System.out.println("Has seleccionado la opcion de ver los libro que hay.\n\n");
 					System.out.println("Introduzca el libro que quieres buscar: ");
-					titulo = varstring.nextLine();
+					titulo = scstring.nextLine();
 					for(int i = 0, contador = 1; i < catalogo.size(); i++, contador++) {
 						if(catalogo.get(i).gettitulo().equals(titulo)) {
 						System.out.println("Libro "+contador+": \n "
@@ -79,7 +72,7 @@ public class biblioteca {
 				case 3:
 					System.out.println("Has seleccionado la opcion de borrar libros.\n ");
 					System.out.println("Introduzca el titulo del libro que quiere borrar: ");
-					titulo = varstring.nextLine();
+					titulo = scstring.nextLine();
 					for(int i = 0; i < catalogo.size(); i++) {
 						if(catalogo.get(i).gettitulo().equals(titulo)) {
 							catalogo.remove(i).gettitulo().equals(titulo);
@@ -92,7 +85,7 @@ public class biblioteca {
 				case 4:
 					System.out.println("Has seleccionado la opcion de hacer un prestamo.");
 					System.out.println("Introduzca el titulo del libro que quiere prestar: ");
-					titulo = varstring.nextLine();
+					titulo = scstring.nextLine();
 					for(int i = 0; i < catalogo.size(); i++) {
 						if(catalogo.get(i).gettitulo().equals(titulo)) {
 							if (catalogo.get(i).getejemplares() > catalogo.get(i).getprestados()){
@@ -109,7 +102,7 @@ public class biblioteca {
 				case 5:
 					System.out.println("Has seleccionado la opcion de devolver un libro:");
 					System.out.println("Introduzca el nombre del libro a devolver:");
-					titulo = varstring.nextLine();
+					titulo = scstring.nextLine();
 					for(int i = 0; i < catalogo.size(); i++) {
 						if(catalogo.get(i).gettitulo().equals(titulo)) {
 							if (catalogo.get(i).getejemplares() > catalogo.get(i).getprestados()){
@@ -127,8 +120,22 @@ public class biblioteca {
 					System.out.println("Hasta luego");
 				}
 		}while(opcion != 0);
-		varstring.close();
-		varint.close();
-		varopcion.close();
+		scstring.close();
+		scint.close();
+		scopcion.close();
+	}
+	
+	private static int menu(Scanner varint) {
+		int opcion;
+		System.out.println("Estas son las opciones a elegir del menu:");
+		System.out.println("[1] Dar de alta un libro");
+		System.out.println("[2] Consultar el listado de libros");
+		System.out.println("[3] Dar de baja un libro");
+		System.out.println("[4] Hacer un el prestamo de un libro");
+		System.out.println("[5] Hacer una devolucion");
+		System.out.println("[0] Salir del programa");
+		opcion = varint.nextInt();
+		
+		return opcion;
 	}
 }
